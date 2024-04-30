34c6ba874dc2b573cefda2bfb60dad2ff2ef1463
Gustavo Hetrich <159450908+Gustavo-Hetrich@users.noreply.github.com>
2024-04-30 17:11:15 -0300

    bat de execução dos downloads de rede

---
+++ Rede/executar.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Rodando script de instalação de programas...
+echo.
+
+REM Definir o caminho completo para o script PowerShell
+set "script_ps1=D:\Rede\programas.ps1"
+
+REM Verificar se o script PowerShell existe
+if not exist "%script_ps1%" (
+    echo O script PowerShell não foi encontrado.
+    pause
+    exit /b
+)
+
+REM Executar PowerShell como administrador
+powershell.exe -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%script_ps1%""'"
---
+++ Rede/programas.ps1
@@ -0,0 +1,21 @@
+# Diretório onde os programas estão localizados
+$netlogon = "\\grupouninter.local\NETLOGON"
+$corp = "\\corp-dfs-01\ANYDESK ENTERPRISE - EXCLUSIVO SUPORTE TECNICO\Users"
+
+# Executando o Script de Instalação Anydesk
+Write-Host "Instalando Anydesk..."
+& "$corp\AnyDesk_Client_V1.0.exe"
+
+# Executando o Script de Instalação Cortex
+Write-Host "Instalando Cortex..."
+& "$netlogon\Cortex\InstallCortex.ps1"
+
+# Executando o Script de Instalação Global
+Write-Host "Instalando Global..."
+& "$netlogon\GlobalProtect\Install Global Protect.ps1"
+
+# Executando o Script de Netskope
+Write-Host "Instalando Netskope..."
+& "$netlogon\Netskope\Netskope.bat"
+
+Write-Host "Todos os programas foram instalados com sucesso!"
