232c30202221d08569ba05cbdfb8a9fbc5d63e71
Gustavo Hetrich <159450908+Gustavo-Hetrich@users.noreply.github.com>
2024-05-02 16:40:51 -0300

    Update fastcorp.py
    adicionado "limpar temps"

--- fastcorp.py
+++ fastcorp.py
@@ -2,11 +2,12 @@ from pynput.mouse import Button, Controller as mousecontrol
 from pynput.keyboard import Key, Listener, Controller
 import subprocess
 import time
+from colored import Fore, Back, Style
 
 keyboard = Controller()
 mouse = mousecontrol()
 
-titulo = (r"""
+titulo = (rf""" {Fore.red}
     ______           __ 
    / ____/___ ______/ /_
   / /_  / __ `/ ___/ __/
@@ -17,89 +18,166 @@ titulo = (r"""
  / /   / __ \/ ___/ __ \
 / /___/ /_/ / /  / /_/ /
 \____/\____/_/  / .___/ 
-               /_/      
+               /_/     
 
-Framework de Assistência ao Suporte Técnico Corporativo""")
+{Style.reset}Framework de Assistência ao Suporte Técnico Corporativo """)
 
-
-def on_release(key):
-    
-    if key == Key.esc:
-        print('parando configuração')
-        return False
 while True:
     print(titulo)
-    inicio = input(f'\n Quer iniciar a configuração? \n (1) Checklist \n favor digitar uma das opções \n \n')
+    inicio = input(f'\n Quer iniciar a configuração? \n (1) Checklist \n (2) Limpar Temps \n Favor digitar uma das opções \n \n')
 
+
+    # Checklist
     if inicio == '1':
-        print('\n começando configuração')
-
-        # Atualização
-        print('\n Atualizando Windows...')
-        keyboard.press(Key.cmd)
-        keyboard.release(Key.cmd)
-        time.sleep(0.5)
-        keyboard.type('atualizações')
-        time.sleep(0.5)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(3)
-        for i in range(0,3):
-            keyboard.press(Key.tab)
-            keyboard.release(Key.tab)
+        modelo = input(f'\n Qual seria o modelo do notebook? \n (1)Acer (2)Outro (3...)Voltar \n Favor digitar uma das opções \n \n')
+        # Acer
+        if modelo == '1':
+            print('Iniciando configuração do Acer')
+            # Atualização
+            print('\n Atualizando Windows...')
+            keyboard.press(Key.cmd)
+            keyboard.release(Key.cmd)
             time.sleep(0.5)
-        time.sleep(2)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(5)
-        # GPUPDATE
-        print('\n Realizando GPUPDATE...')
-        keyboard.press(Key.cmd)
-        keyboard.release(Key.cmd)
-        time.sleep(0.5)
-        keyboard.type('gpupdate /force /boot /sync')
-        time.sleep(0.5)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(10)
-        # Ativação Administrador
-        print('\n Ativando Administrador...')
+            keyboard.type('atualizações')
+            time.sleep(0.5)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(3)
+            for i in range(0,3):
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+                time.sleep(0.5)
+            time.sleep(2)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(5)
+            # GPUPDATE
+            print('\n Realizando GPUPDATE...')
+            keyboard.press(Key.cmd)
+            keyboard.release(Key.cmd)
+            time.sleep(0.5)
+            keyboard.type('gpupdate /force /boot /sync')
+            time.sleep(0.5)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(10)
+            # Ativação Administrador
+            print('\n Ativando Administrador...')
+            keyboard.press(Key.cmd)
+            keyboard.press('r')
+            time.sleep(0.5)
+            keyboard.release(Key.cmd)
+            keyboard.release('r')
+            time.sleep(1)
+            keyboard.tap(Key.backspace)
+            keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Ativador Admin')
+            time.sleep(0.5)
+            keyboard.press(Key.ctrl)
+            keyboard.press(Key.shift)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.ctrl)
+            keyboard.release(Key.shift)
+            keyboard.release(Key.enter)
+            time.sleep(30)
+            # Instalação Cortex, Global, Netskope
+            print('\n Instalando Programas...')
+            keyboard.press(Key.cmd)
+            keyboard.press('r')
+            time.sleep(0.5)
+            keyboard.release(Key.cmd)
+            keyboard.release('r')
+            time.sleep(2)
+            keyboard.tap(Key.backspace)
+            keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Rede\executar')
+            time.sleep(2)
+            keyboard.tap(Key.enter)
+            time.sleep(0.5)
+           
+            print('\n Checklist Concluido')
+            
+
+        # Outro
+        if modelo == '2':
+            # Atualização
+            print('\n Atualizando Windows...')
+            keyboard.press(Key.cmd)
+            keyboard.release(Key.cmd)
+            time.sleep(5)
+            keyboard.type('atualizações')
+            time.sleep(5)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(20)
+            for i in range(0,3):
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+                time.sleep(1)
+            time.sleep(5)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(10)
+            # GPUPDATE
+            print('\n Realizando GPUPDATE...')
+            keyboard.press(Key.cmd)
+            keyboard.release(Key.cmd)
+            time.sleep(2)
+            keyboard.type('gpupdate /force /boot /sync')
+            time.sleep(2)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(20)
+            # Ativação Administrador
+            print('\n Ativando Administrador...')
+            keyboard.press(Key.cmd)
+            keyboard.press('r')
+            time.sleep(2)
+            keyboard.release(Key.cmd)
+            keyboard.release('r')
+            time.sleep(4)
+            keyboard.tap(Key.backspace)
+            keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Ativador Admin')
+            time.sleep(2)
+            keyboard.press(Key.ctrl)
+            keyboard.press(Key.shift)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.ctrl)
+            keyboard.release(Key.shift)
+            keyboard.release(Key.enter)
+            time.sleep(60)
+            # Instalação Cortex, Global, Netskope
+            print('\n Instalando Programas...')
+            keyboard.press(Key.cmd)
+            keyboard.press('r')
+            time.sleep(2)
+            keyboard.release(Key.cmd)
+            keyboard.release('r')
+            time.sleep(4)
+            keyboard.tap(Key.backspace)
+            keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Rede\executar')
+            time.sleep(4)
+            keyboard.tap(Key.enter)
+            print('\n Checklist Concluido')
+
+    #Limpar Temps
+    if inicio == '2':
+        print('\nLimpando Temps...')
         keyboard.press(Key.cmd)
         keyboard.press('r')
         time.sleep(0.5)
         keyboard.release(Key.cmd)
         keyboard.release('r')
-        time.sleep(1)
+        time.sleep(2)
         keyboard.tap(Key.backspace)
-        keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Ativador Admin')
-        time.sleep(0.5)
+        # https://stackoverflow.com/questions/10716803/batch-file-to-perform-start-run-temp-and-delete-all - fonte do Bat
+        keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Clear.bat')
+        time.sleep(2)
         keyboard.press(Key.ctrl)
         keyboard.press(Key.shift)
         keyboard.press(Key.enter)
         keyboard.release(Key.ctrl)
         keyboard.release(Key.shift)
         keyboard.release(Key.enter)
-        time.sleep(30)
-        # Instalação Cortex, Global, Netskope
-        print('\n Instalando Programas...')
-        keyboard.press(Key.cmd)
-        keyboard.press('r')
-        time.sleep(0.5)
-        keyboard.release(Key.cmd)
-        keyboard.release('r')
-        time.sleep(2)
-        keyboard.tap(Key.backspace)
-        keyboard.type('Y:\SUPORTE TECNICO\Corporativo\Macros\Bot Format\Rede\executar')
-        time.sleep(2)
-        keyboard.tap(Key.enter)
-        time.sleep(0.5)
-        mouse.position = (1103, 605)
-        mouse.press(Button.left)
-        mouse.release(Button.left)
-        keyboard.tap(Key.down)
-        keyboard.tap(Key.up)
-        keyboard.tap(Key.enter)
-        print('\n Checklist Concluido')
-        print('-' * 50)
+        print('\nTemps limpas')
 
-     
+    print('\n')     
+    print('-' * 50)
